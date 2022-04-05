5f6bbccf5fc01cb30930788f2969f2e67315aff6
LDprg <71488985+LDprg@users.noreply.github.com>
2022-04-05 19:50:19 +0200

    fix docker files

--- src/index.php
+++ src/index.php
@@ -1,3 +1,23 @@
 <?php
-echo "Hello there, this is a PHP Apache container";
-?>
+class DB {
+		private static $_db_username = "MYSQL_USER";
+		private static $_db_password = "MYSQL_PASSWORD";
+		private static $_db_host = "db";
+		private static $_db_name = "MYSQL_DATABASE";
+		private static $_db;
+
+
+		function __construct() {
+			try {
+			self::$_db = new PDO("mysql:host=" . self::$_db_host . ";dbname=" . self::$_db_name,  self::$_db_username , self::$_db_password);
+			} catch(PDOException $e) {
+				echo "Datenbankverbindung fehlgeschlagen!<br>";
+				die($e);
+			}
+		}
+
+	}
+	$db = new DB();
+
+
+?> 
